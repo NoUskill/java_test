4ed00a1944e6825cedf1d3124f39ba9709f2a0de
NoUskill <154542414+NoUskill@users.noreply.github.com>
2024-11-05 18:55:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,70 +1,79 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
-class NodeBinTree{
-    public Integer data;
-    public NodeBinTree left_child;
-    public NodeBinTree right_child;
-    public NodeBinTree parent;
 
+import com.google.gson.annotations.SerializedName;
 
-    public NodeBinTree(){
-        this.data= null;
-        this.left_child= null;
-        this.right_child=null;
-        this.parent=null;
-    }
-    public NodeBinTree(int num){
-        this.data= num;
-        this.left_child= null;
-        this.right_child=null;
-        this.parent=null;
+class wik_page{
+    int ns;
+    String title;
+    int pagid;
+    int size;
+    int wordcount;
+    String snippet;
+    String timestamp;
+    void print(){
+        System.out.println(this.ns);
+        System.out.println(this.title);
+        System.out.println(this.pagid);
+        System.out.println(this.size);
+        System.out.println(this.wordcount);
+        System.out.println(this.snippet);
+        System.out.println(timestamp);
     }
 }
 
-class _BinTree{
-    public NodeBinTree head;
-    public _BinTree(int num){
-        NodeBinTree temp=new NodeBinTree(num);
-        this.head=temp;
+
+
+
+class ansverJson{
+    String batchcomplete;
+    @SerializedName("continue") con con;
+    class con{
+        int sroffset;
+        @SerializedName("continue") String c;
+
     }
-    public void Append(int num){
-        NodeBinTree temp = new NodeBinTree(num);
-
-        NodeBinTree temp_node=this.head;
-        NodeBinTree last_node=temp_node;
-
-        while (temp_node != null){
-            last_node=temp_node;
-            if(temp_node.data > temp.data) {
-                temp_node=temp_node.left_child;
-            }
-            else {
-                temp_node=temp_node.right_child;
-            }
-        }
-        temp_node= temp;
-        if(last_node.data > temp.data){
-            last_node.left_child=temp_node;
-        }else{
-            last_node.right_child=temp_node;
-        }
-        temp_node.parent=last_node;
-    }
-
-
-
 
 }
+
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        _BinTree Test=new _BinTree(15);
-        Test.Append(10);
-        Test.Append(25);
-        Test.Append(5);
-        System.out.println(Test.head.data);
-        System.out.println(Test.head.left_child.data);
-        System.out.println(Test.head.right_child.data);
-        System.out.println(Test.head.left_child.left_child.data);
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
+        Scanner in=new Scanner(System.in);
+        String str = in.nextLine();
+        in.close();
+        String urlAddres;
+        urlAddres="https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=";
+        str=URLEncoder.encode(str, StandardCharsets.UTF_8);
+
+        urlAddres=urlAddres+str;
+
+        URL home = new URI(urlAddres).toURL();
+        System.out.println(home);
+        HttpURLConnection connection=(HttpURLConnection) home.openConnection();
+        connection.setRequestMethod("GET");
+        int rescode=connection.getResponseCode();
+        System.out.println(rescode);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        StringBuilder response = new StringBuilder();
+        while((line=reader.readLine())!=null){
+            System.out.println(line);
+            System.out.println("\n");
+            response.append(line);
+        }
+        reader.close();
 
     }
 }
